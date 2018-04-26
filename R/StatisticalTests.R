fddd360a463c8d7ac5ee5b0570d1c702906bf51d
gonzaparra <parra.gonzalo@gmail.com>
2018-04-26 20:07:22 +0200

    DE genes function implemented

--- R/StatisticalTests.R
+++ R/StatisticalTests.R
@@ -124,3 +124,113 @@ GetGeneCorrelationNetwork <-function(ExpressionMatrix, cor_threshold=0.4, plot=T
   return(GeneCorrelations)
 }
 
+
+
+#' Filter Differentially Expressed Genes
+#'
+#' Given an expression matrix it keeps the differentially expressed genes in the data. First, genes expressed in less than 5% of the cells are filtered out.
+#' Then a PCA is applied on the data. Next (optionally) a tSNE reduction is applied using the PCs components that were kept from the step before.
+#' Clusters are foudn in the data, using the mclust package. The number of clusters is defined by using the BIC method.
+#' Finally a kruskal.test is iteratively applied to find the differentially expressed genes in the data (e-val > 0.001)
+#'
+#' @param ExpressionMatrix contains the expression profiles(columns) along the cells (rows)
+#' @param NPCs Number of principal components to be kept for clustering the data.
+#'
+#' @return DE_genes list of differentially expressed genes
+#'
+#'
+#' @export
+#'
+FilterGenes<-function(ExprMatrix, NPCs=10)
+{
+  # testing
+  # ExprMatrix=Dataset$ExpressionMatrix
+  # end testing
+
+  keep_genes=c()
+  # Filter Genes without expression in at least 5% of cells
+  for(i in 1:dim(ExprMatrix)[2])
+  {
+    if(length(which(ExprMatrix[,i]==0)) < dim(ExprMatrix)[1]*0.05 )
+    {
+      keep_genes=c(keep_genes, i)
+    }
+  }
+
+  ExprMatrix=ExprMatrix[,keep_genes]
+  # Calculate PCA for the data, keeping the first 50 componentes
+  PCATransform=PCA(log2(t(ExprMatrix)+1), ncp = NPCs, graph = F)
+  # Select those components that capture 60% of the variability
+  # PCsToUse=which(PCATransform$eig[,3]<60)
+  PCsToUse=1:NPCs
+  ReducedSpace=PCATransform$var$coord[,PCsToUse]
+
+  library(tsne)
+  ReducedSpace=tsne(ReducedSpace)
+
+  # library(Rtsne)
+  # ReducedSpace=Rtsne(ReducedSpace, )
+  # ReducedSpace=ReducedSpace$Y
+
+
+  # We cluster the data with mclust
+  library(mclust)
+  # We decide the number of clusters to calculate with the BIC criteria
+  mclusters=mclustBIC(ReducedSpace)
+
+  # Cluster the data
+  print("Clustering...")
+  Clustering <- Mclust(ReducedSpace, x = mclusters)
+  print(paste("Found ", length(unique(Clustering$classification)), "clusters.."))
+
+  DE_genes=c()
+
+  for(Cluster_i in 1:length(unique(Clustering$classification)))
+  {
+    print(paste("Finding DE genes in cluster", Cluster_i, sep=" "))
+    DE_genes_clusteri=DE_genes_cluster(Cluster_i, Clustering, ExprMatrix)
+    DE_genes=c(DE_genes, DE_genes_clusteri$GeneName[which(DE_genes_clusteri$evals<0.001)])
+  }
+
+  DE_genes=unique(DE_genes)
+
+  return(DE_genes)
+}
+
+DE_genes_cluster <- function(Cluster, Clustering, ExprMatrix)
+{
+  # testing
+  # Cluster=1
+  # ExprMatrix=Dataset$ExpressionMatrix
+  # end testing
+
+  CellsClusteri=which(Clustering$classification==Cluster)
+  AllOtherCells=which(!(1:dim(ExprMatrix)[1] %in% CellsClusteri))
+
+  statistic_vector=c()
+
+  for(Gene_j in 1:dim(ExprMatrix)[2])
+  {
+    # vectors for the expression values
+    # Gene_j=1
+    GenePopulation1=ExprMatrix[CellsClusteri, Gene_j]
+    GenePopulation2=ExprMatrix[AllOtherCells, Gene_j]
+
+    BothPopulations <- data.frame(Y=c(GenePopulation1, GenePopulation2), group=c(rep("Population1", length(GenePopulation1)), rep("Population2", length(GenePopulation2))))
+    KruskalResult=kruskal.test(Y~group, data=BothPopulations)
+
+    statistic_vector=c(statistic_vector, KruskalResult$p.value)
+  }
+
+  statistic_vector
+
+  genes_significances=sort(statistic_vector, index.return=T)
+  names(genes_significances)[names(genes_significances)=="x"] <- "pvals"
+  names(genes_significances)[names(genes_significances)=="ix"] <- "indexes"
+  genes_significances$GeneName <- colnames(ExprMatrix)[genes_significances$indexes]
+  genes_significances$evals <- genes_significances$pvals * length(genes_significances$pvals)
+
+  return(genes_significances)
+}
+
+
