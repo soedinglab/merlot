6e357dcb430721ac703c3f76608c63801cf67f85
Robrecht Cannoodt <brechtus@gmail.com>
2018-06-11 18:03:47 +0200

    wip fix reinstall

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,20 @@
 #' @importFrom glue glue
 .onLoad <- function(libname, pkgname) {
-  path <- find.package('merlot')
-  if(!dir.exists(glue::glue("{path}/venv"))) {
+  if (!dir.exists(system.file(package = "merlot", "venv"))) {
     reinstall()
   }
 }
 
 #' @importFrom glue glue
 reinstall <- function() {
-  path <- find.package('merlot')
-  system(glue::glue("bash {path}/make {path}/venv"))
+  is_checking <- find.package("merlot") != system.file(package = "merlot")
+  if (is_checking) {
+    venv_path <- tempfile("venv")
+    on.exit(unlink(venv_path, recursive = TRUE, force = TRUE))
+  } else {
+    venv_path <- system.file(package = "merlot", "venv")
+  }
+
+  make_path <- system.file(package = "merlot", "make")
+  system(glue::glue("bash {make_path} {venv_path}"))
 }
