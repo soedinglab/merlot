c508df8b575eab82ef6903d4bc8800c4aa15b124
Robrecht Cannoodt <brechtus@gmail.com>
2018-06-11 18:26:19 +0200

    fix reinstall function

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 #' @importFrom glue glue
 .onLoad <- function(libname, pkgname) {
-  if (!dir.exists(system.file(package = "merlot", "venv"))) {
+  if (!dir.exists(venv_path())) {
     reinstall()
   }
 }
@@ -8,13 +8,20 @@
 #' @importFrom glue glue
 reinstall <- function() {
   is_checking <- find.package("merlot") != system.file(package = "merlot")
+
+  # if the package is being run during an R CMD CHECK,
+  # the venv needs to be created in a temporary folder
   if (is_checking) {
     venv_path <- tempfile("venv")
     on.exit(unlink(venv_path, recursive = TRUE, force = TRUE))
   } else {
-    venv_path <- system.file(package = "merlot", "venv")
+    venv_path <- venv_path()
   }
 
   make_path <- system.file(package = "merlot", "make")
   system(glue::glue("bash {make_path} {venv_path}"))
+}
+
+venv_path <- function() {
+  paste0(find.package("merlot"), "/venv")
 }
